570a063a13ef89beca1f58027143285b9ae0b460
Ian Menezes <ianmenezes35@gmail.com>
2023-07-06 10:30:53 -0300

    Interfaces DepartmentDao and SellerDao

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -2,6 +2,7 @@ package model.entities;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 public class Seller implements Serializable {
 
@@ -76,10 +77,7 @@ public class Seller implements Serializable {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -91,12 +89,7 @@ public class Seller implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Seller other = (Seller) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+		return Objects.equals(id, other.id);
 	}
 
 	@Override
